f075efdcd96f31a8e52934e530eeea9a08683188
reqadh <128897272+reqadh@users.noreply.github.com>
2024-03-10 23:40:00 +0300

    Add files via upload

---
+++ GOODBYE.py
@@ -0,0 +1,28 @@
+#import math
+#print(dir(math))
+
+#import os
+#os.mkdir('pictures')
+#os.chdir('pictures')
+
+#print(os.getcwd())
+
+
+#try:
+ #   raise Exception(1, 2, 3)
+#except Exception as e:
+ #   print(len(e.args))
+
+
+ #ابراهيم## جالس يكتب التقييم
+
+
+#صح#### تشوفينه كيف منحاس
+
+#import calendar
+#calendar.setfirstweekday(calendar.SUNDAY)
+#print(calendar.weekheader(3))
+
+print(chr(ord('p') +2))
+
+
